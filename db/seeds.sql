1d7e21676e5232a4637940c77509999c77fd07a0
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-06-01 21:25:02 -0500

    Created First Files, App Setup Done, DB Setup Done and Config Setup Done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Royal with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon');
